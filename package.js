b340beda8f7e5554f2fa3a3449501cb6a98f89df
Serhii Chaichuk <128085928+chaichuk@users.noreply.github.com>
2025-11-25 23:20:12 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "dtek-to-cloudflare",
+  "version": "1.0.0",
+  "main": "sync.js",
+  "type": "module",
+  "scripts": {
+    "start": "node sync.js"
+  },
+  "dependencies": {
+    "playwright": "^1.49.0",
+    "undici": "^6.0.0"
+  }
+}
